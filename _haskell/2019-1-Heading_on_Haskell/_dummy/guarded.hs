27d5afa3b97f181d8786ea5d917cee4f2077182e
devJRL <dev2sponge@gmail.com>
2019-10-27 19:55:11 +0900

    remove previous setting

--- _haskell/2019-1-Heading_on_Haskell/_dummy/guarded.hs
+++
@@ -1,3 +0,0 @@
-classifier k | k < 150 = "short"
-	     | k < 180 = "normal"
-	     | otherwise = "tall"
--- _haskell/2019-1-Heading_on_Haskell/jeongryeol/2/patternMatching.hs
+++
@@ -1,4 +0,0 @@
-exPatMat :: Int -> Int -> Int -> Int
-exPatMat 0 1 0 = 999       -- Arg. Pattern
-exPatMat 1 1 1 = 1         -- Arg. Pattern
-exPatMat x y z = x + y + z -- otherwise..
