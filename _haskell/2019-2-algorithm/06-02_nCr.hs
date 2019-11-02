9772986a4cc09e2b62a7144fe734207912889368
devJRL <dev2sponge@gmail.com>
2019-11-02 06:41:54 +0000

    [SOLVED] nCm : functional thinking

--- _haskell/2019-2-algorithm/06-02_nCr.hs
+++ _haskell/2019-2-algorithm/06-02_nCr.hs
@@ -1,75 +1,57 @@
--- | n개의 원소 중 m개를 고르는 모든 조합을 찾는 알고리즘
+ -- | n개의 원소 중 m개를 고르는 모든 조합을 찾는 알고리즘
+  
+ import Data.List
+ import Data.Void
 
-import Data.List
-import Data.Void
-
-main :: IO()
-main = do
-    putStrLn $ ""
-    putStrLn $ "n개의 원소 중 m개를 고르는 모든 조합을 찾는 알고리즘"
-    putStrLn $ "===================================================="
-    
-    putStr   $ "n = "
-    input'n <- getLine
-    putStr   $ "m = "
-    input'm <- getLine
-    putStrLn $ "----------------------------------------------------"
-    putStrLn $ "n = " ++ input'n ++ " / " ++ " m = " ++ input'm 
-    putStrLn $ "===================================================="
-    putStrLn $ ""
-    
-    result <- return $ count'combi (read input'n) (read input'm)
-    putStrLn $ "조합의 수 : " ++ show result ++ "\n"
-    putStrLn $ "----------------------------------------------------"
-    
-    putStrLn $ ""
-    putStrLn $ "[1,2,3,4,5] 에서 3개를 뽑는 조합 찾기 데모"
-    putStrLn $ ""
-    putStrLn $ "----------------------------------------------------"
-    
-    putStrLn $ ""
-    result'list <- return $ extrc'combi [1,2,3,4,5,6] 3 
-    putStrLn $ "조합의 출력 >> " ++ "\n" ++ show result'list ++ "\n"
-    putStrLn $ ""
+ main :: IO()
+ main = do
+     putStrLn $ "----------------------------------------------------"
+     putStr   $ "기본 리스트를 입력하세요 = "
+     input'list <- getLine
+     putStr   $ "뽑을 개수를 입력하세요 = "
+     m <- getLine
+     putStrLn $ "----------------------------------------------------"
+     putStrLn $ ""
+     putStrLn $ input'list ++ " 에서 " ++ m ++  " 개를 뽑는 조합 찾기"
+     putStrLn $ ""
+     putStrLn $ "----------------------------------------------------"
+     putStrLn $ ""
+     result'list <- return $ exec'combi (read input'list) $ read m
+     putStrLn $ "조합의 출력 >> " ++ "\n" ++ show result'list
+     putStrLn $ ""
+     putStrLn $ "----------------------------------------------------"
 
 
--- | 조합의 수
-count'combi :: Int -> Int -> Int
-count'combi n m
-    | n == m          = 1
-    | n < 0 || m < 0  = 0
-    | n < m           = 0
-    | otherwise       = n * count'combi (n - 1) m
+ -- | 조합의 수
+ count'combi :: Int -> Int -> Int
+ count'combi n m
+     | n == m          = 1
+     | n < 0 || m < 0  = 0
+     | n < m           = 0
+     | otherwise       = n * count'combi (n - 1) m
 
 
--- | Lv.1 | 원소팩을 지정개수로 조합
-extrc'combi :: [Int] -> Int -> [[Int]]
-extrc'combi pack@(p:ps) num
-    | num <= 0          = [[]]    -- 선택없음
-    | check' pack num   = [pack]  -- 원소팩 전체 선택              
-    | otherwise         =
-            head'resrv pack num   -- [>>] 원소팩의 헤드 고정 조합 생성
-            ++ extrc'combi ps num -- [>>] 원소팩의 헤드 제외 후 재귀
+ -- | 전체 원소집합에서 n개의 조합을 찾아 출력하는 함수
+ exec'combi :: [Int] -> Int -> [[Int]]
+ exec'combi lists@(l:ls) m
+     | length lists <  m  = [[   ]]
+     | length lists == m  = [lists]
+     | m == 1             = [[x] | x <- lists ]
+     | otherwise          = fix'head l ls (m-1) ++ exec'combi ls m
 
--- | Lv.2 | 원소팩의 헤드 고정하여 조합
-head'resrv :: [Int] -> Int -> [[Int]]
-head'resrv pack@(a:as) num =           -- 앵커(a) : 원소팩의 헤드
-    let result = pick'tails as $ num-1 -- 결과조합: 원소팩의 테일로 만든 조합
-    in  [a : rs | rs <- result]        -- [>>] 앵커와 결과조합을 최종조합
+ -- | 헤드를 반드시 포함하는 조합을 찾는 함수//  head ++ [n-1개를 뽑아서 만든 조합]
+ fix'head :: Int -> [Int] -> Int -> [[Int]]
+ fix'head fix tails@(t:ts) count
+     | length tails <= count  = [ [fix] ++ tails ]
+     | otherwise              = [ fix : x | x <- picked ] ++ fix'head fix ts count
+     where picked = pick'tails tails count
 
--- | Lv.3 | 앵커를 뺀 원소팩의 테일조합
-pick'tails :: [Int] -> Int -> [[Int]]
-pick'tails tail@(t:ts) num
-    | check' tail num  = [tail]       -- 원소팩의 길이와 지정개수가 동일하면 팩 리턴
-    | otherwise        =
-        let scope = num - 1           -- 고정 크기
-            prev  = take scope tail   -- 고정 구간
-            loop  = drop scope tail   -- 변화 구간
-        in  [prev ++ [x] | x <- loop] -- [>>] 변화 구간에서 추출한 원소를 고정구간과 조합
-            ++ pick'tails ts num      -- [>>] 원소팩의 테일을 재귀호출
-
-
--- | util | 원소팩의 길이와 지정개수 비교
-check' :: [Int] -> Int -> Bool
-check' pack num =
-    if (length pack == num) then True else False
+ -- | n-1개에 대한 조합을 생성하는 함수
+ pick'tails :: [Int] -> Int -> [[Int]]
+ pick'tails scope count
+     | length scope == count  = [scope]
+     | otherwise              = [picked ++ [select] | select <- option]
+     where
+         point  = count-1
+         picked = take point scope
+         option = drop point scope
