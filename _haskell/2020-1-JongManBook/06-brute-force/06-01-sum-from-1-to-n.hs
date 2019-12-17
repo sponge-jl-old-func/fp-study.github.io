90de10a42bcb4516f46465e1443dfd7c89bf141c
Jeongryeol <jeongryeol@Jeongryeolui-MacBook-Pro.local>
2019-12-17 22:58:13 +0900

    코드 6.1 / 1 ~ n 합, 반복함수와 재귀함수

---
+++ _haskell/2020-1-JongManBook/06-brute-force/06-01-sum-from-1-to-n.hs
@@ -0,0 +1,11 @@
+-- | [JongManBook] Algorithm Problem Solving Strategies (book-1)
+-- |  06. 무식하게 풀기
+-- | #01. 1부터 n까지 합을 계산하는 반복함수와 재귀함수
+
+-- | 조건: n >= 1
+-- | 결과: 1부터 n까지의 합을 반환한다.
+-- | 함수: int sum(int n)
+
+-- | 조건: n >= 1
+-- | 결과: 1부터 n까지의 합을 반환한다.
+-- | 함수: int recursive(int n)
